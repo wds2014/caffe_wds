dc88e7e899d96ad65dc8d4fb234907c5d38ecabd
wds2014 <hellowds2014@gmail.com>
2018-02-03 22:46:29 +0800

    make some changes

---
+++ mnist/creat_net.py
@@ -0,0 +1,104 @@
+import sys
+sys.path.insert(0,'/home/dana/caffe/python')
+import caffe
+from caffe import layers as L, params as P
+from caffe.proto import caffe_pb2
+import glob
+import numpy as np
+import sys
+import os.path
+
+base_root='/home/dana/caffe_wds/mnist'
+train_data=os.path.join(base_root,'data/mnist_train_lmdb')
+test_data=os.path.join(base_root,'data/mnist_test_lmdb')
+train_net=os.path.join(base_root,'train.prototxt')
+test_net=os.path.join(base_root,'test.prototxt')
+deploy_net=os.path.join(base_root,'deploy.prototxt')
+solver_path=os.path.join(base_root,'solver.prototxt')
+snap_path=os.path.join(base_root,'snap/')
+
+
+def creat_train_net(train_data, batch_size):
+    n=caffe.NetSpec()
+    n.data,n.label=L.Data(source=train_data,batch_size=batch_size,ntop=2,
+                   backend=P.Data.LMDB,transform_param=dict(scale=1./255))
+    n.conv1=L.Convolution(n.data,kernel_size=5,stride=1,num_output=20,
+             weight_filler=dict(type='xavier'))
+    n.pool1=L.Pooling(n.conv1,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.conv2=L.Convolution(n.pool1,kernel_size=5,stride=1,num_output=50,
+             weight_filler=dict(type='xavier'))
+    n.pool2=L.Pooling(n.conv2,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.fc1=L.InnerProduct(n.pool2,num_output=500,weight_filler=dict(type='xavier'))
+    n.relu1=L.ReLU(n.fc1,in_place=True)
+    n.fc2=L.InnerProduct(n.fc1,num_output=10,weight_filler=dict(type='xavier'))
+    n.loss=L.SoftmaxWithLoss(n.fc2,n.label)
+    return n.to_proto()
+
+def creat_test_net(test_data, batch_size):
+    n=caffe.NetSpec()
+    n.data,n.label=L.Data(source=test_data,batch_size=batch_size,ntop=2,
+                   backend=P.Data.LMDB,transform_param=dict(scale=1./255))
+    n.conv1=L.Convolution(n.data,kernel_size=5,stride=1,num_output=20,
+             weight_filler=dict(type='xavier'))
+    n.pool1=L.Pooling(n.conv1,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.conv2=L.Convolution(n.pool1,kernel_size=5,stride=1,num_output=50,
+             weight_filler=dict(type='xavier'))
+    n.pool2=L.Pooling(n.conv2,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.fc1=L.InnerProduct(n.pool2,num_output=500,weight_filler=dict(type='xavier'))
+    n.relu1=L.ReLU(n.fc1,in_place=True)
+    n.fc2=L.InnerProduct(n.fc1,num_output=10,weight_filler=dict(type='xavier'))
+    n.acc=L.Accuracy(n.fc2,n.label)
+    return n.to_proto()
+
+def creat_deploy_net():
+    n=caffe.NetSpec()
+    n.conv1=L.Convolution(bottom='data',kernel_size=5,stride=1,num_output=20,
+             weight_filler=dict(type='xavier'))
+    n.pool1=L.Pooling(n.conv1,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.conv2=L.Convolution(n.pool1,kernel_size=5,stride=1,num_output=50,
+             weight_filler=dict(type='xavier'))
+    n.pool2=L.Pooling(n.conv2,kernel_size=2,stride=2,pool=P.Pooling.MAX)
+    n.fc1=L.InnerProduct(n.pool2,num_output=500,weight_filler=dict(type='xavier'))
+    n.relu1=L.ReLU(n.fc1,in_place=True)
+    n.fc2=L.InnerProduct(n.fc1,num_output=10,weight_filler=dict(type='xavier'))
+    n.prob=L.Softmax(n.fc2)
+    return n.to_proto()
+
+def write_deploy(deploy_path):
+    with open(deploy_path,'w') as f:
+        f.write('name:"mnist_net"\n')
+        f.write('input:"data"\n')
+        f.write('input_dim:1\n')
+        f.write('input_dim:1\n')
+        f.write('input_dim:28\n')
+        f.write('input_dim:28\n')
+        f.write(str(creat_deploy_net()))
+
+def create_solver(train_net,test_net,snap_path):
+    s=caffe_pb2.SolverParameter()
+    s.random_seed=0xCAFFE
+    s.train_net=train_net
+    s.test_net.append(test_net)
+    s.test_interval=500
+    s.test_iter.append(100)
+    s.max_iter=20000
+    s.type="SGD"
+    s.base_lr=0.01
+    s.momentum=0.9
+    s.weight_decay=5e-4
+    s.lr_policy='inv'
+    s.gamma=0.0001
+    s.power=0.75
+    s.display=200
+    s.snapshot=1000
+    s.snapshot_prefix=snap_path
+    s.solver_mode=caffe_pb2.SolverParameter.CPU
+    return s
+
+with open(train_net,'w') as f:
+    f.write(str(creat_train_net(train_data,64)))
+with open(test_net,'w') as f:
+    f.write(str(creat_test_net(test_data,100)))
+write_deploy(deploy_net)
+with open(solver_path,'w') as f:
+    f.write(str(create_solver(train_net,test_net,snap_path)))
--- mnist/mnist-test.py
+++ mnist/mnist-test.py
@@ -24,15 +24,22 @@ image=cv2.imread('./pics/wds3.png')
 image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
 image3=cv2.resize(image,(28,28),interpolation=cv2.INTER_CUBIC)
 
+image=cv2.imread('./pics/00024.png')
+image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+image4=cv2.resize(image,(28,28),interpolation=cv2.INTER_CUBIC)
+
 #images=np.array([image,image1,image2]).reshape(3,1,28,28)
 #print(images.shape)
+#image1=np.array(image1,dtype=np.float32)
+#image2=np.array(image2,dtype=np.float32)
+#image3=np.array(image3,dtype=np.float32)
+image4=np.array(image4,dtype=np.float32)/255
 
-
-model_def ='./lenet_deploy.prototxt'
+model_def ='./deploy.prototxt'
 model_weights='./pretrained.caffemodel'
 net=caffe.Net(model_def,model_weights,caffe.TEST)
 
-net.blobs['data'].data[0,0,:,:]=image3
+net.blobs['data'].data[0,0,:,:]=image4
 #net.blobs['data'].data[1,0,:,:]=image2
 #net.blobs['data'].data[2,0,:,:]=image3
 #net.blobs['data'].data[0,0]=np.zeros((28,28))
@@ -41,5 +48,5 @@ output=net.forward()
 print(output)
 output_prob=net.blobs['prob'].data[0]
 print(output_prob)
-cv2.imshow('image',image3)
+cv2.imshow('image',image4)
 cv2.waitKey(0)
